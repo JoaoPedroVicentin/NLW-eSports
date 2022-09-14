e307809575f4a7d5614895f2e01f506d4c6e9f23
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2022-09-13 23:17:34 -0300

    Aula02 v2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,34 @@
 import './styles/main.css'
+import logoImg from './assets/logo-nlw-esports.svg'
 
 function App() {
-
   return (
-    <div className='w-8 h-8 bg-violet-500'>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt=''/>
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.
+      </h1>
 
+      <div className='grid grid-cols-6 gap-6'>
+        <a href=''>
+          <img src='/image 1.png' alt=''/>
+        </a>
+        <a href=''>
+          <img src='/image 2.png' alt=''/>
+        </a>
+        <a href=''>
+          <img src='/image 3.png' alt=''/>
+        </a>
+        <a href=''>
+          <img src='/image 5.png' alt=''/>
+        </a>
+        <a href=''>
+          <img src='/image 6.png' alt=''/>
+        </a>
+        <a href=''>
+          <img src='/image 7.png' alt=''/>
+        </a>
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '.src/**/*.tsx',
-    './index.html'
-  ],
+  content: ['./src/**/*.tsx', './index.html'],
   theme: {
     extend: {
       backgroundImage: {
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%);',
         galaxy: "url('/background-galaxy.png')"
       }
     },
