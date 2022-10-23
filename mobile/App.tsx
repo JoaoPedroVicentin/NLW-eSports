d9ff9e66bf5e4d06dcf30703db5f142aae74518b
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2022-10-23 19:42:25 -0300

    .

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'react-native'
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter'
-
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
+import { useRef, useEffect } from 'react';
 import { Background } from './src/components/Background';
 import { Routes } from './src/Routes';
 import { Loading } from './src/components/Loading';
+import { Subscription } from 'expo-modules-core'
+import './src/service/notificationConfigs'
+import { getPushNotificationToken } from './src/service/getPushNotificationToken'
+import * as Notifications from 'expo-notifications'
 
 export default function App() {
+
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  })
+
   const [fontsLoade] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -13,10 +25,27 @@ export default function App() {
     Inter_900Black
   })
 
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  }, [])
+
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoade ?  <Routes /> : <Loading />}
+      {fontsLoade ? <Routes /> : <Loading />}
     </Background>
   );
 }
